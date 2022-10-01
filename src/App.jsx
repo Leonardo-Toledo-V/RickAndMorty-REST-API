922059be98d9e8cf0b98da8b7973317faa2c1545
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2022-09-30 23:36:26 -0500

    primeros pasos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Character from './components/Character';
+import CharacterList from './components/CharacterList';
+function App() {
+
+  return (
+    <div>
+      <h1>Rick and morty</h1>
+        <CharacterList/>
+    </div>
+
+  );
+}
+
+export default App;
+
